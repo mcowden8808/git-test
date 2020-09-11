cdd6455775ea41af45743f994d3983f73d1c7bf2
mcowden8808 <mcowden808@yahoo.com>
2020-09-11 02:26:28 -0400

    second commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let sanitizeHTML = require('sanitize-html')
 let app = express()
 let db
 
-let port = process.env.port
+let port = process.env.PORT
 if (port == null || port == "") {
   port = 3000
 }
